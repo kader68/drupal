964299077488116cb6554bcf24640cdf1e74ee83
kaderhod <kaderhood@free.fr>
2019-04-24 16:32:00 +0200

    Mise à jour config prod

--- deploid_prod.sh
+++ deploid_prod.sh
@@ -7,6 +7,11 @@ composer install
 # Mettre à jour la base de données Drupal
 drush updb -y
 
+#Ajout des config de pro
+git add config/prod
+git commit -m 'Mise à jour des config de prod'
+git push origin master
+
 # Importer les configurations
 drush cim -y
 
